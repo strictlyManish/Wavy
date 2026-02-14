e3a42c149899a2d1b5c45a906f45a2ff36e0f4bb
strictlyManish <rajzmanish94@gmail.com>
2026-02-14 22:28:54 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="bg-black text-white overflow-hidden select-none">
+    <div className="bg-gray-950 text-white overflow-hidden select-none">
       <Home />
     </div>
   );
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import { LoaderCircle, LucideLoaderCircle } from "lucide-react";
 
 function Home() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>
-        <div>
-            
-        </div>
+    <div className="h-screen w-screen flex justify-center flex-col gap-5 items-center">
+      <img src="logo.png" alt="logo" className="h-15" />
+      <p className="font-black">
+        <LucideLoaderCircle className="rotate-90 animate-spin h-6" />
+      </p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
