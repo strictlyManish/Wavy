8108e96f0bb1f4dc06ef9b2b90a20fc5e0644588
strictlyManish <rajzmanish94@gmail.com>
2026-02-14 22:15:16 +0530

    it's time to make fronted for login+register functionlity check

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="bg-black text-white overflow-hidden select-none">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
