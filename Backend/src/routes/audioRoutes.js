98655f08727d9040046e95599110a0136e14ccf1
strictlyManish <rajzmanish94@gmail.com>
2026-02-20 22:34:17 +0530

    Re-Initiating new project

--- Backend/src/routes/audioRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const authMiddlleware = require("../middlewares/auth.middleware");
-const UploadAudioController = require("../controller/audio.controller");
-const routes = express.Router();
-
-routes.post("/", authMiddlleware, UploadAudioController);
-
-module.exports = routes;
